4e9a7dfebebee03ce59472a7ce3195724f5b5edc
ddr4x <chen.shawn123@gmail.com>
2023-06-13 22:46:07 +0900

    update player class

--- src/Player.py
+++ src/Player.py
@@ -14,20 +14,20 @@ from pygame.locals import (
 
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self, keyBinds):
         super(Player, self).__init__()
-
+        self.keyBinds = keyBinds
         self.surf = pygame.image.load("src/capy.jpeg")
         self.surf = pygame.transform.scale(self.surf, (50, 50)) # scale image down
         self.rect = self.surf.get_rect()
     def update(self, pressed_keys, dt):
-        if pressed_keys[K_UP]:
+        if pressed_keys[self.keyBinds["up"]]:
             self.rect.move_ip(0, -3 * dt)
-        if pressed_keys[K_DOWN]:
+        if pressed_keys[self.keyBinds["down"]]:
             self.rect.move_ip(0, 2 * dt)
-        if pressed_keys[K_LEFT]:
+        if pressed_keys[self.keyBinds["left"]]:
             self.rect.move_ip(-2 * dt, 0)
-        if pressed_keys[K_RIGHT]:
+        if pressed_keys[self.keyBinds["right"]]:
             self.rect.move_ip(2 * dt, 0)
          # Keep player on the screen
         if self.rect.left < 0:
@@ -39,4 +39,4 @@ class Player(pygame.sprite.Sprite):
         if self.rect.bottom >= C.SCREEN_HEIGHT:
             self.rect.bottom = C.SCREEN_HEIGHT
     def gravity(self):
-        self.rect.move_ip(0,20) # how fast player falls
+        self.rect.move_ip(0,C.GRAVITY) # how fast player falls
