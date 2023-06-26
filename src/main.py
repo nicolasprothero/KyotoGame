4a15706981a18d663f0dee491ed0935b8cb04f97
Nicolas Prothero <nprothero0917@gmail.comgit config --global user.name Nicolas Prothero>
2023-06-26 18:10:37 +0900

    another test for author

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ from game import Game
 
 # we are all set if we have this message
 # and this message too!!! - nic
-# testing merges three
+# testing merges four
 def main():
     game = Game()
     game.run_menu()
